eefaef8da36bf338ac4094becc1627b572112764
tjasad <tjasa.domadenik10@gmail.com>
2022-11-06 16:17:30 +0100

    Initial commit

---
+++ src/test/java/si/fri/rso/GreetingResourceIT.java
@@ -0,0 +1,8 @@
+package si.fri.rso;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+public class GreetingResourceIT extends GreetingResourceTest {
+    // Execute the same tests but in packaged mode.
+}
